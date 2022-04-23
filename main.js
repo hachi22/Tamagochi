09145d53c1b34b1462ba2320215b03af82034839
unknown <adrimart212@gmail.com>
2022-04-23 21:58:02 +0200

    acciones hechas, faltaria el juego en si y estetica de botones

---
+++ main.js
@@ -0,0 +1,57 @@
+var atrasJugar = document.getElementById("playAtras");
+var jugar = document.getElementById("jugar");
+var atrasTrabajar = document.getElementById("trabajarAtras");
+var trabajar = document.getElementById("trabajo");
+var atrasPasear = document.getElementById("pasearAtras");
+var pasear = document.getElementById("pasear");
+var atrasDormir = document.getElementById("dormirAtras");
+var dormir = document.getElementById("dormir");
+
+
+atrasJugar.onclick = () => {
+	document.getElementById("divPlay").style.display = "none";
+	document.getElementById("mainScreenContainer").style.display = "block";
+}
+
+jugar.onclick = () => {
+	document.getElementById("divPlay").style.display = "block";
+	document.getElementById("mainScreenContainer").style.display = "none";
+}
+
+atrasTrabajar.onclick = () => {
+	document.getElementById("divTrabajar").style.display = "none";
+	document.getElementById("mainScreenContainer").style.display = "block";
+}
+
+trabajar.onclick = () => {
+	document.getElementById("divTrabajar").style.display = "block";
+	document.getElementById("mainScreenContainer").style.display = "none";
+}
+
+atrasPasear.onclick = () => {
+	document.getElementById("divPasear").style.display = "none";
+	document.getElementById("mainScreenContainer").style.display = "block";
+	document.getElementById("htmlId").style.backgroundColor = "#6ecccc";
+}
+
+pasear.onclick = () => {
+	document.getElementById("divPasear").style.display = "block";
+	document.getElementById("mainScreenContainer").style.display = "none";
+	document.getElementById("htmlId").style.backgroundColor = "black";
+}
+
+atrasDormir.onclick = () => {
+	document.getElementById("divDormir").style.display = "none";
+	document.getElementById("mainScreenContainer").style.display = "block";
+	document.getElementById("htmlId").style.backgroundColor = "#6ecccc";
+}
+
+dormir.onclick = () => {
+	document.getElementById("divDormir").style.display = "block";
+	document.getElementById("mainScreenContainer").style.display = "none";
+	document.getElementById("htmlId").style.backgroundColor = "black";
+}
+	
+	
+	
+
